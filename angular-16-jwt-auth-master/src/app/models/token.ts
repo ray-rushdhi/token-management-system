b98c5d85572a5f6802af795abec8f46d1ecc0603
Rayyan Rushdhi <ahamedrayyan06@gmail.com>
2023-08-24 13:18:14 +0530

    Docker support

---
+++ angular-16-jwt-auth-master/src/app/models/token.ts
@@ -0,0 +1,17 @@
+import { TokenState } from "./tokenState.enum";
+
+export interface Token{
+    id?: string;
+    tokenNum?: number;
+    selectedDay?: string;
+    state?: TokenState;
+    reservedByID?: number;
+    reservedByName?: string;
+}
+
+export class TokenImplementation implements Token {
+    constructor(
+      public selectedDay?: string,
+      public reservedByID?: number
+    ) {}
+  }
